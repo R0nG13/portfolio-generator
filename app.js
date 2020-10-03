09a2ba22e7f27cecdab06ea5ec28704c2afb886a
R0nG13 <67929861+R0nG13@users.noreply.github.com>
2020-10-03 13:11:38 -0400

    Capture User input

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-var message = 'Hello Node!';
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-var sum = 5 + 3;
+// const printProfileData = (profileDataArr) => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//   console.log(profileDataArr[i]);
+//   }
+// };
 
-console.log(message);
-console.log(sum);
+// printProfileData(profileDataArgs);
 
+const printProfileData = profileDataArr => {
+  // This...
+  // for (let i = 0; i < profileDataArr.length; i += 1) {
+  //   console.log(profileDataArr[i]);
+  // }
+
+  // console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
